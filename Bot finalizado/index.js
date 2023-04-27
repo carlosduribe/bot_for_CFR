a6bd4f6841ac495fdbca27abd6cad5f7f7348037
Carlos Daniel Uribe <carlosduribe@hotmail.com>
2023-04-27 18:14:06 -0500

    Extraidos varias subs, derma e interna presentan problemas

--- Bot finalizado/index.js
+++ Bot finalizado/index.js
@@ -13,9 +13,10 @@ puppeteer.use(stealthPlugin());
   await page.type('#username', 'ocristianandres_0495@hotmail.com', { delay: 100 });
   await page.type('#password', 'CRFR100*', { delay: 100 });
   await page.click('button[type="submit"]');
+  await page.waitForSelector('text/Crist');
   console.log("Logged in succesfully");
 
-  const subespecialidad = 'https://app.cursofuturosresidentes.com/courses/cirugia-general/'
+  const subespecialidad = 'https://app.cursofuturosresidentes.com/courses/medicina-interna/'
   await page.goto(subespecialidad);
   await page.waitForSelector('text/Expandir todo')
   await page.click('text/Expandir todo')
@@ -27,23 +28,23 @@ puppeteer.use(stealthPlugin());
       .map((anchor) => anchor.href)
   );
   console.log("topics extracted");
-  fs.writeFile('topics.txt', topics.join('\n'), (err) => {
-    if (err) throw err;
-    console.log('Topics saved to file!');
-  });
 
   const videos = [];
   for (const topic of topics) {
     await page.goto(topic);
     const html = await page.content();
     const regex = /https:\/\/player\.vimeo\.com\/video\/\w{9}/;
-    const match = html.match(regex)
-    
+    const match = html.match(regex);
     videos.push(match[0]);
     
   };
   console.log('videos extracted');
   await browser.close();
+
+  fs.writeFile('topics.txt', topics.join('\n'), (err) => {
+    if (err) throw err;
+    console.log('Topics saved to file!');
+  });
 
   fs.writeFile('videos.txt', videos.join('\n'), (err) => {
     if (err) throw err;
