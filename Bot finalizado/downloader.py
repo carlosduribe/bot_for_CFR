f427bc129eda5e8692e51b3d57ec2c6cb7ca70ee
Carlos Daniel Uribe <carlosduribe@hotmail.com>
2023-04-27 17:23:07 -0500

    Carga carpeta de cirugía

--- Bot finalizado/downloader.py
+++ Bot finalizado/downloader.py
@@ -16,12 +16,12 @@ for i in range(len(topics)):
   videos_and_topics.append((topics[i], videos[i]))
 
 for i in videos_and_topics:
-  v = Vimeo(i)
+  v = Vimeo(videos_and_topics[i])
   stream = v.streams  
       
   for s in stream:
       if s.quality == '720p':
-          s.download(download_directory='video', filename=i[1][47:])
+          s.download(download_directory='video', filename=videos_and_topics[i][1][47:])
           break
       else:  
           print('quality not found')
